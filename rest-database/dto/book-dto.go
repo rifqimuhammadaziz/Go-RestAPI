78b332216bcca9c768784ca4a9a7854dddedc39a
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-09-13 12:58:27 +0700

    create dto

--- rest-database/dto/book-dto.go
+++ rest-database/dto/book-dto.go
@@ -1,1 +1,16 @@
 package dto
+
+// BookUpdateDTO is used by client to update the book
+type BookUpdateDTO struct {
+	ID          uint64 `json:"id" form:"id" binding:"required"`
+	Title       string `json:"title" form:"title" binding:"required"`
+	Description string `json:"description" form:"description" binding:"required"`
+	UserID      uint64 `json:"user_id,omitempty" form:"user_id,omitempty"`
+}
+
+// BookCreateDTO is used by client to create new the book
+type BookCreateDTO struct {
+	Title       string `json:"title" form:"title" binding:"required"`
+	Description string `json:"description" form:"description" binding:"required"`
+	UserID      uint64 `json:"user_id,omitempty" form:"user_id,omitempty"`
+}
---
+++ rest-database/dto/login-dto.go
@@ -0,0 +1,7 @@
+package dto
+
+// LoginDTO is used when client request POST method from /login URL
+type LoginDTO struct {
+	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
+	Password string `json:"password" form:"password" binding:"required" validate:"min:6"`
+}
