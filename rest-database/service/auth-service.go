5b3be9027452663a0d15865290d0eb53167b68bf
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-09-15 09:05:42 +0700

    create auth service

---
+++ rest-database/service/auth-service.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"Go-RestAPI/rest-database/dto"
+	"Go-RestAPI/rest-database/entity"
+	"Go-RestAPI/rest-database/repository"
+	"github.com/mashingan/smapping"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
+
+// AuthService is a contract about this service what can do
+type AuthService interface {
+	VerifyCredential(email string, password string) interface{}
+	CreateUser(user dto.UserCreateDTO) entity.User
+	FindByEmail(email string) entity.User
+	IsDuplicateEmail(email string) bool
+}
+
+type authService struct {
+	userRepository repository.UserRepository
+}
+
+// NewAuthService is used to create new instance of AuthService
+func NewAuthService(userRepo repository.UserRepository) AuthService {
+	return &authService{
+		userRepository: userRepo,
+	}
+}
+
+func (service *authService) VerifyCredential(email string, password string) interface{} {
+	res := service.userRepository.VerifyCredential(email, password)
+	if v, ok := res.(entity.User); ok {
+		comparedPassword := comparePassword(v.Password, []byte(password))
+		if v.Email == email && comparedPassword {
+			return res
+		}
+		return false
+	}
+	return res
+}
+
+func (service *authService) CreateUser(user dto.UserCreateDTO) entity.User {
+	userToCreate := entity.User{}
+	err := smapping.FillStruct(&userToCreate, smapping.MapFields(&user))
+	if err != nil {
+		log.Fatalf("Failed to map %v", err)
+	}
+	res := service.userRepository.InsertUser(userToCreate)
+	return res
+}
+
+func (service *authService) FindByEmail(email string) entity.User {
+	return service.userRepository.FindByEmail(email)
+}
+
+func (service *authService) IsDuplicateEmail(email string) bool {
+	res := service.userRepository.IsDuplicateEmail(email)
+	return !(res.Error == nil)
+
+}
+
+func comparePassword(hashedPwd string, plainPwd []byte) bool {
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
