804d569eb9e9816f9a0aa069825663c128ffd781
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-09-16 11:01:26 +0700

    test user module

--- rest-database/dto/user-dto.go
+++ rest-database/dto/user-dto.go
@@ -2,7 +2,7 @@ package dto
 
 // UserUpdateDTO is used by client when request PUT method profile
 type UserUpdateDTO struct {
-	ID       uint64 `json:"id" form:"id" binding:"required"`
+	ID       uint64 `json:"id" form:"id"`
 	Name     string `json:"name" form:"name" binding:"required"`
 	Email    string `json:"email" form:"name" binding:"required,email"`
 	Password string `json:"password,omitempty" form:"password,omitempty" binding:"min=6"`
--- rest-database/entity/user.go
+++ rest-database/entity/user.go
@@ -6,5 +6,5 @@ type User struct {
 	Name     string `gorm:"type:varchar(255)" json:"name"`
 	Email    string `gorm:"uniqueIndex;type:varchar(255)" json:"email"`
 	Password string `gorm:"->;<-;not null" json:"-"`
-	Token    string `gorm:"-" json:"token"`
+	Token    string `gorm:"-" json:"-"`
 }
