0c999556af92829ee0be3f34016666e10cfee02f
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-09-16 13:35:29 +0700

    create book repository & service

---
+++ rest-database/service/book-service.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"Go-RestAPI/rest-database/dto"
+	"Go-RestAPI/rest-database/entity"
+	"Go-RestAPI/rest-database/repository"
+	"fmt"
+	"github.com/mashingan/smapping"
+	"log"
+)
+
+type BookService interface {
+	Insert(b dto.BookCreateDTO) entity.Book
+	Update(b dto.BookCreateDTO) entity.Book
+	Delete(b entity.Book)
+	All() []entity.Book
+	FindByID(bookID uint64) entity.Book
+	IsAllowedToEdit(userID string, bookID uint64) bool
+}
+
+type bookService struct {
+	bookRepository repository.BookRepository
+}
+
+// NewBookService creates new instance of BookService
+func NewBookService(bookRepo repository.BookRepository) BookService {
+	return &bookService{
+		bookRepository: bookRepo,
+	}
+}
+
+func (service *bookService) Insert(b dto.BookCreateDTO) entity.Book {
+	book := entity.Book{}
+	err := smapping.FillStruct(&book, smapping.MapFields(&b))
+	if err != nil {
+		log.Fatalf("Failed to map %v: ", err)
+	}
+	res := service.bookRepository.InsertBook(book)
+	return res
+}
+
+func (service *bookService) Update(b dto.BookCreateDTO) entity.Book {
+	book := entity.Book{}
+	err := smapping.FillStruct(&book, smapping.MapFields(&b))
+	if err != nil {
+		log.Fatalf("Failed to map %v: ", err)
+	}
+	res := service.bookRepository.UpdateBook(book)
+	return res
+}
+
+func (service *bookService) Delete(b entity.Book) {
+	service.bookRepository.DeleteBook(b)
+}
+
+func (service *bookService) All() []entity.Book {
+	return service.bookRepository.AllBook()
+}
+
+func (service *bookService) FindByID(bookID uint64) entity.Book {
+	return service.bookRepository.FindBookByID(bookID)
+}
+
+func (service *bookService) IsAllowedToEdit(userID string, bookID uint64) bool {
+	b := service.bookRepository.FindBookByID(bookID)
+	id := fmt.Sprintf("%v", b.UserID)
+	return userID == id
+}
